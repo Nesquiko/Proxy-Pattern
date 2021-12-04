12f440250fb6296116f0704e925dc38fb85ad6be
Nesquiko <lukascastven@gmail.com>
2021-12-04 22:06:47 +0100

    Add upgrade helper function.

--- scripts/util.py
+++ scripts/util.py
@@ -1,4 +1,6 @@
 from brownie import network, accounts, config
+from brownie.network.contract import ProjectContract
+from brownie.project.main import new
 import eth_utils
 
 
@@ -26,3 +28,39 @@ def encode_function_data(initializer=None, *args):
         eth_utils.to_bytes(hexstr="0x")
 
     return initializer.encode_input(*args)
+
+
+def upgrade(
+    account,
+    proxy: ProjectContract,
+    new_impl_address: ProjectContract,
+    proxy_admin=None,
+    initializer=None,
+    *args
+):
+    tx = None
+
+    if proxy_admin:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            tx = proxy_admin.upgradeAndCall(
+                proxy.address,
+                new_impl_address.address,
+                encoded_function_call,
+                {"from": account},
+            )
+        else:
+            tx = proxy_admin.upgrade(
+                proxy.address, new_impl_address.address, {"from": account}
+            )
+
+    else:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            tx = proxy_admin.upgradeToAndCall(
+                new_impl_address.address, encoded_function_call, {"from": account}
+            )
+        else:
+            tx = proxy.upgradeTo(new_impl_address.address, {"from": account})
+
+    return tx
